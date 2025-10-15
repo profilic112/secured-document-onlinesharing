6d4e004e9a5a1eeddc79d5963c132bf3c9f7d0e3
profilic112 <admin@felmx.com>
2025-10-15 17:21:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 // email-redirect.js
 
 
-
-const urlParams = new URLSearchParams(window.location.search);
-const verified = urlParams.get("verified");
-if (verified !== "true") {
-    alert("Please complete verification first.");
-    window.location.href = "verify.html";
-
 document.addEventListener("DOMContentLoaded", function() {
     // Disable right-click
     document.addEventListener("contextmenu", function(e) {
